b585f1cf943c8fca0cd6e768ee2f501a18f95577
Buford Eeds <bufordeeds8@gmail.com>
2020-01-20 20:04:12 -0600

    drew route for sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users
       resources :games
+      resources :sessions, only: [:create, :destroy]
+      root to:'static#home'
     end
   end
 
