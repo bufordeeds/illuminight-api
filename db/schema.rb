a8c1d9d687cecb34cdcbda08d905b67965823e07
Buford Eeds <bufordeeds8@gmail.com>
2020-01-20 20:04:42 -0600

    updated users table to have pw column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_17_154120) do
+ActiveRecord::Schema.define(version: 2020_01_20_194850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_01_17_154120) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "games", "users"
